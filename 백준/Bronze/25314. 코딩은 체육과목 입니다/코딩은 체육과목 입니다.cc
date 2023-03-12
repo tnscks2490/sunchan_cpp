0fbf46518960b07b47559f241fdbc072f4e06e4c
tnscks2490 <112551499+tnscks2490@users.noreply.github.com>
2023-03-12 12:29:14 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int N;
+	cin >> N;
+	N = N / 4;
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << "long ";
+	}
+	cout << "int" << endl;
+	return 0;
+}
