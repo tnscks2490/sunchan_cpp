7820a26e4431c7d154fc9aaa83dd06134a9c0b56
tnscks2490 <112551499+tnscks2490@users.noreply.github.com>
2023-03-26 15:29:11 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	int arr[9] = { 0, };
+	int result[7] = { 0, };
+	int sum = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		cin >> arr[i];
+		sum = sum + arr[i];
+	}
+
+	sum = sum - 100;
+	int a = 0, b = 0;
+	bool t = true;
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = i; j < 9; j++)
+		{
+			if (arr[i] + arr[j] == sum) {
+				if (i != j)
+				{
+					a = i;
+					b = j;
+					t = false;
+				}
+			}
+		}
+		if (t = false)
+		{
+			break;
+		}
+	}
+	int index = 0;
+	for (int k = 0; k < 9; k++)
+	{
+
+		if (k != a and k != b)
+		{
+			result[index] = arr[k];
+			index++;
+		}
+	}
+	sort(result, result + 7);
+	for (int o = 0; o < 7; o++)
+	{
+		cout << result[o] << endl;
+	}
+	return 0;
+}
