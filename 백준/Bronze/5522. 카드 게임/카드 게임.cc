c2d8f67a95609b03d61eefdf198ef158cfb46fa6
tnscks2490 <112551499+tnscks2490@users.noreply.github.com>
2023-04-01 01:31:17 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int sum = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		int a;
+		cin >> a;
+		sum = sum + a;
+	}
+
+	cout << sum << endl;
+	return 0;
+}
