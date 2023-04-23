a0f3873c943bfcc16af12c0ab94b1fa9177f944a
tnscks2490 <112551499+tnscks2490@users.noreply.github.com>
2023-04-23 19:29:15 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int L, P;
+	cin >> L >> P;
+	int sum = L * P;
+	int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+	cout << a-sum << " ";
+	cout << b-sum << " ";
+	cout << c-sum << " ";
+	cout << d-sum << " ";
+	cout << e-sum << " ";
+
+
+
+	return 0;
+}
