0160e6f06f83e7d657d11b1dec80ae8b233e9ac5
tnscks2490 <112551499+tnscks2490@users.noreply.github.com>
2023-04-29 00:32:14 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 1; i <= 2 * N - 1; i++)
+	{
+		if (i < N)
+		{
+			for (int k = 0; k < i; k++)
+			{
+				cout << "*";
+			}
+			cout << endl;
+		}
+		else if (i == N)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				cout << "*";
+			}
+			cout << endl;
+		}
+		else
+		{
+			for (int k = 0; k < 2 * N - i; k++)
+			{
+				cout << "*";
+			}
+			cout << endl;
+		}
+	}
+	return 0;
+}
