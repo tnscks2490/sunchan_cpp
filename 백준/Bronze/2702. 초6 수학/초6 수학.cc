dee7c9c65a25079fdad800293eb047edd282e9a5
tnscks2490 <112551499+tnscks2490@users.noreply.github.com>
2023-06-11 16:41:58 +0900

    [Bronze II] Title: 초6 수학, Time: 60 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2702. 초6 수학/초6 수학.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int get_max(int a, int b);
+int get_min(int a, int b);
+int main() {
+	int TC;
+	cin >>  TC;
+	for (int i = 0; i < TC; i++)
+	{
+		int N, M;
+		cin >> N >> M;
+		cout << get_min(N, M) <<  " " << get_max(N, M) << endl;
+	}
+
+	return 0;
+}
+
+int get_max(int N, int M)
+{
+	int range = min(M, N);
+	int tmp = 1;
+
+	for (int i = 1; i <= range; i++)
+	{
+		if (N % i == 0 and M % i == 0)
+		{
+			tmp = i;
+		}
+	}
+	return tmp;
+}
+
+int get_min(int N, int M)
+{
+	int small = min(M, N);
+	int big = max(M, N);
+
+	int s = small;
+	int b = big;
+	while (s != b)
+	{
+		if (s < b)
+		{
+			s += small;
+		}
+		else
+		{
+			b = b += big;
+		}
+	}
+	return s;
+}
