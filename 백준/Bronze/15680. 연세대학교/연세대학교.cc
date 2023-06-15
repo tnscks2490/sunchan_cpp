1e1e55b5193055d05a0cd2f9976d691c4517e02e
tnscks2490 <112551499+tnscks2490@users.noreply.github.com>
2023-06-15 23:58:52 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int N;
+	cin >> N;
+	if (N == 0)
+	{
+		cout << "YONSEI" << endl;
+	}
+	else if (N == 1)
+	{
+		cout << "Leading the Way to the Future" << endl;
+	}
+	return 0;
+}
