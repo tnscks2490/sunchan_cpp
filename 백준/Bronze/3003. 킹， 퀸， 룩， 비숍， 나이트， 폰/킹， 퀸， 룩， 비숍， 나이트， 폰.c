5eafc83c8768eb846121a25ccff7fab779fd5c13
tnscks2490 <112551499+tnscks2490@users.noreply.github.com>
2023-08-22 23:46:09 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 int main()
 {
-    int C[6] = {0,};
-    int a;
+    int C[6]; //= {0,};
+
     for (int i = 0; i < 6;i++) {
-        scanf("%d", &a);
-        C[i] = a;
+        scanf("%d", &C[i]);
+        //C[i] = a;
     }
     printf("%d %d %d %d %d %d", 1 - C[0], 1 - C[1], 2 - C[2], 2 - C[3], 2 - C[4], 8 - C[5]);
     return 0;
