3eec7f17a4692f40927dc51a9a1e418db9032f69
tnscks2490 <112551499+tnscks2490@users.noreply.github.com>
2023-08-28 15:30:13 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 680 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	int N;
+	cin >> N;
+	int* arr = new int[N] {0, };
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	for (int i = N-1; i >= 0; i--)
+	{
+		cout << arr[i] << "\n";
+	}
+	return 0;
+}
